207c502715846f844b32cd4755fc6a0ab63fd458
Shane Rosse <rosse@usc.edu>
2016-04-08 20:43:47 -0700

    Initial Commit

---
+++ LatePlate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LatePlate
+//
+//  Created by Shane Rosse on 4/8/16.
+//  Copyright © 2016 Shane Rosse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
