fc3c4e0c7775bc9ce8a9a9e0207d0a0c8fd7d4ce
Shane Rosse <rosse@usc.edu>
2016-04-21 18:06:47 -0700

    LatePlate

--- LatePlate/ViewController.swift
+++ LatePlate/ViewController.swift
@@ -7,19 +7,64 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var signupButton: UIButton!
+    @IBOutlet var backgroundView: UIView!
+    
+    let db_url: String = "https://blazing-heat-9345.firebaseio.com"
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let tapGesture: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        self.backgroundView.addGestureRecognizer(tapGesture)
     }
-
+    
+    func dismissKeyboard() {
+        nameField.resignFirstResponder()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func signupTouch(sender: AnyObject) {
+        
+        let myRootRef = Firebase(url: db_url)
+        let myUserRef = myRootRef.childByAppendingPath("users")
+        
+        if let name = nameField.text {
+            let uniqueRef = myUserRef.childByAutoId()
+            self.dismissKeyboard()
+            uniqueRef.setValue(name, withCompletionBlock: { (error, firebase) in
+                var message: String = ""
+                if (error != nil) {
+                    message = "Something went wrong in our database..."
+                    print("This is the Firebase error: \(error)")
+                } else {
+                    message = "You're signed up!"
+                }
+                let alertController = UIAlertController(title: "Late Plate", message: message, preferredStyle: UIAlertControllerStyle.Alert)
+                
+                let okAction = UIAlertAction(title: "OK", style: .Default, handler: { (UIAlertAction) in
+                    self.dismissKeyboard()
+                })
+                
+                alertController.addAction(okAction)
+                self.presentViewController(alertController, animated: true, completion: {
+                    
+                })
+            })
+        }
+        
+    }
 }
 
---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '8.0'
+# Uncomment this line if you're using Swift
+
+use_frameworks!
+
+target 'LatePlate' do
+
+pod 'Firebase', '>= 2.5.1'
+
+end
+
