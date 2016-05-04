6737130edf718acc6a7a3c60c7ef9064a01baa10
Shane Rosse <rosse@usc.edu>
2016-05-04 14:51:25 -0700

    Updated feedback and design

---
+++ LatePlate/FeedbackVC.swift
@@ -0,0 +1,35 @@
+//
+//  FeedbackVC.swift
+//  LatePlate
+//
+//  Created by Shane Rosse on 5/3/16.
+//  Copyright © 2016 Shane Rosse. All rights reserved.
+//
+
+import UIKit
+
+class FeedbackVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
